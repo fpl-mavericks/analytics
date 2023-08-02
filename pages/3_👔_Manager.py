82e0c6f5d999a71205c77440f4c810251be5ad68
timyouell-servian <tim.youell@servian.com>
2023-08-02 10:37:15 +1000

    more error handling

--- pages/3_👔_Manager.py
+++ pages/3_👔_Manager.py
@@ -193,23 +193,24 @@ with col3:
 
 if fpl_id == '':
     st.write('')
-elif len(gw_complete_list) == 0:
-    st.write('')
 else:
     curr_df = pd.DataFrame(get_manager_history_data(fpl_id)['current'])
-    man_data = get_manager_details(fpl_id)
-    curr_df['Manager'] = man_data['player_first_name'] + ' ' + man_data['player_last_name']
-    ave_df = pd.DataFrame(get_bootstrap_data()['events'])[['id', 'average_entry_score']]
-    ave_df.columns=['event', 'points']
-    ave_df['Manager'] = 'GW Average'
-    ave_cut = ave_df.loc[ave_df['event'] <= max(curr_df['event'])]
-    concat_df = pd.concat([curr_df, ave_cut])
-    c = alt.Chart(concat_df).mark_line().encode(
-        x=alt.X('event', axis=alt.Axis(tickMinStep=1, title='GW')),
-        y=alt.Y('points', axis=alt.Axis(title='GW Points')),
-        color='Manager').properties(
-            height=400)
-    st.altair_chart(c, use_container_width=True)
+    if len(curr_df) == 0:
+        st.write('')
+    else:
+        man_data = get_manager_details(fpl_id)
+        curr_df['Manager'] = man_data['player_first_name'] + ' ' + man_data['player_last_name']
+        ave_df = pd.DataFrame(get_bootstrap_data()['events'])[['id', 'average_entry_score']]
+        ave_df.columns=['event', 'points']
+        ave_df['Manager'] = 'GW Average'
+        ave_cut = ave_df.loc[ave_df['event'] <= max(curr_df['event'])]
+        concat_df = pd.concat([curr_df, ave_cut])
+        c = alt.Chart(concat_df).mark_line().encode(
+            x=alt.X('event', axis=alt.Axis(tickMinStep=1, title='GW')),
+            y=alt.Y('points', axis=alt.Axis(title='GW Points')),
+            color='Manager').properties(
+                height=400)
+        st.altair_chart(c, use_container_width=True)
 
 def collate_manager_history(fpl_id):
     df = pd.DataFrame(get_manager_history_data(fpl_id)['current'])
@@ -219,8 +220,6 @@ def collate_manager_history(fpl_id):
 
 
 if fpl_id == '':
-    st.write('')
-elif len(gw_complete_list) == 0:
     st.write('')
 else:
     if 'ID' not in st.session_state:
