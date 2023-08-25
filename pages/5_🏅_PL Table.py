1caa7dbbca9c1b27b0623c4a529f2c13e4036f0f
timyouell-servian <tim.youell@servian.com>
2023-08-25 11:32:14 +1000

    test case added

--- pages/5_🏅_PL Table.py
+++ pages/5_🏅_PL Table.py
@@ -20,7 +20,7 @@ base_url = 'https://fantasy.premierleague.com/api/'
 st.set_page_config(page_title='PL Table', page_icon=':sports-medal:', layout='wide')
 define_sidebar()
 
-st.title('Premier League Table')
+st.title('Premier League Table Test')
 
 league_df = get_league_table()
 
