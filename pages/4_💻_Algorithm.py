3edc4853a4be4855484d61409468a5205a698f96
timyouell-servian <tim.youell@servian.com>
2024-01-09 15:35:32 +1100

    GW drop-down added to replace slider

--- pages/4_💻_Algorithm.py
+++ pages/4_💻_Algorithm.py
@@ -27,7 +27,7 @@ st.write('Gradient Boosting Regressor Algorithm trained on 22/23 historic and 23
 crnt_gw = get_current_gw()
 
 preds_df = pd.read_csv('./data/2023_24_pred_file.csv')
-preds_df.rename(columns={'xP': 'Pred_Pts'}, inplace=True)
+preds_df.rename(columns={'xP': 'xPts'}, inplace=True)
 
 ele_df = remove_moved_players(pd.DataFrame(get_bootstrap_data()['elements']))
 ele_types_df = pd.DataFrame(get_bootstrap_data()['element_types'])
@@ -42,7 +42,9 @@ def get_home_away_str_dict():
         values = list(new_fdr_df[col])
         max_length = new_fixt_df[col].str.len().max()
         if max_length > 7:
-            new_fixt_df.loc[new_fixt_df[col].str.len() <= 7, col] = new_fixt_df[col].str.pad(width=max_length+9, side='both', fillchar=' ')
+            new_fixt_df.loc[new_fixt_df[col].str.len() <= 7, col] = new_fixt_df[col].str.pad(width=max_length+9,
+                                                                                             side='both',
+                                                                                             fillchar=' ')
         strings = list(new_fixt_df[col])
         value_dict = {}
         for value, string in zip(values, strings):
@@ -97,7 +99,8 @@ cols = st.columns([8, 1, 8])
 team_cols = st.columns([1])
 second_cols = st.columns([8,1,8])
 
-gw_slider = cols[0].selectbox('Select GW: ', range(int(crnt_gw), int(gw_max)))
+gw_slider = cols[0].multiselect('Select GW: ', list(range(int(crnt_gw), int(gw_max+1))), [int(crnt_gw)])
+
 filter_pos = cols[2].multiselect(
         'Filter Position',
         ['GKP', 'DEF', 'MID', 'FWD'],
@@ -114,42 +117,55 @@ filter_team = team_cols[0].multiselect(
 
 price_min = (ele_df['now_cost'].min())/10
 price_max = (ele_df['now_cost'].max())/10
-slider1, slider2 = second_cols[0].slider('Filter Price: ', price_min, price_max, [price_min, price_max], 0.1, format='£%.1f')
+slider1, slider2 = second_cols[0].slider('Filter Price: ', price_min, price_max,
+                                         [price_min, price_max], 0.1, format='£%.1f')
 
 tsb_min = (ele_df['selected_by_percent'].astype(float).min())
 tsb_max = (ele_df['selected_by_percent'].astype(float).max())
-tsb_slid1, tsb_slid2 = second_cols[2].slider('Filter TSB%: ', tsb_min, tsb_max, [tsb_min, tsb_max], 0.1, format='%.1f')
+tsb_slid1, tsb_slid2 = second_cols[2].slider('Filter TSB%: ', tsb_min, tsb_max,
+                                             [tsb_min, tsb_max], 0.1, format='%.1f')
 
-df = preds_df.loc[preds_df['GW'] == gw_slider]
+df = preds_df.loc[preds_df['GW'].isin(gw_slider)]
+
+# For each element, add together xPts for multiple GWs
+df.columns.tolist()
+df = df[['element', 'xPts']].groupby('element').sum()
 
 ele_df['Pos'] = ele_df['element_type'].map(ele_types_df.set_index('id')['singular_name_short'])
 ele_df['team'] = ele_df['team'].map(teams_df.set_index('id')['short_name'])
 ele_df.rename(columns={'id': 'element',
-                       'total_points': 'Total_Pts',
+                       'total_points': 'Total Pts',
                        'news': 'News',
                        'selected_by_percent': 'TSB%'}, inplace=True)
 ele_df['Name'] = ele_df['element'].map(player_dict)
 ele_df['Price'] = ele_df['now_cost']/10
 
 team_fdr_df, team_fixt_df, team_ga_df, team_gf_df = get_fixt_dfs()
-new_fixt_df = team_fixt_df.loc[:, gw_slider:(gw_slider)]
+new_fixt_df = team_fixt_df.loc[:, gw_slider]
+new_fdr_df = team_fdr_df.loc[:, gw_slider]
 new_fixt_cols = ['GW' + str(col) for col in new_fixt_df.columns.tolist()]
 new_fixt_df.columns = new_fixt_cols
-new_fdr_df = team_fdr_df.loc[:, gw_slider:(gw_slider)]
+new_fdr_df.columns = new_fixt_cols
 
-league_df = league_df.join(new_fixt_df).reset_index()
-league_cut = league_df.copy()[['team'] + new_fixt_cols]
 for col in new_fixt_cols:
-    if league_df[col].dtype == 'O':
-        max_length = league_df[col].str.len().max()
+    if new_fixt_df[col].dtype == 'O':
+        max_length = new_fixt_df[col].str.len().max()
         if max_length > 7:
-            league_df.loc[league_df[col].str.len() <= 7, col] = league_df[col].str.pad(width=max_length+9, side='both', fillchar=' ')
+            new_fixt_df.loc[new_fixt_df[col].str.len() <= 7, col] = new_fixt_df[col].str.pad(width=max_length+9,
+                                                                                             side='both',
+                                                                                             fillchar=' ')
 merge_df = ele_df.merge(df, on='element', how='left')
 
-full_df = merge_df.merge(league_cut, on='team', how='left')
-full_df.sort_values('Pred_Pts', ascending=False, inplace=True)
+new_fixt_df.reset_index(inplace=True)
+new_fixt_df.rename(columns={0: 'team'}, inplace=True)
 
-full_cut = full_df[['Name', 'Pos', 'TSB%', 'News', 'Total_Pts', 'Price', 'Pred_Pts', 'team'] + new_fixt_cols].set_index('Name').copy()
+full_df = merge_df.merge(new_fixt_df, on='team', how='left')
+full_df.sort_values('xPts', ascending=False, inplace=True)
+
+full_df.rename(columns={'team': 'Team'}, inplace=True)
+
+full_cut = full_df[['Name', 'Pos', 'Team', 'TSB%', 'News',
+                    'Total Pts', 'Price', 'xPts'] + new_fixt_cols].set_index('Name').copy()
 full_cut['TSB%'] = full_cut['TSB%'].astype(float)/100
 
 final_df = full_cut.loc[(full_cut['Price'] <= slider2) &
@@ -157,13 +173,14 @@ final_df = full_cut.loc[(full_cut['Price'] <= slider2) &
                         (full_cut['TSB%'] <= tsb_slid2/100) &
                         (full_cut['TSB%'] > tsb_slid1/100) &
                         (full_cut['Pos'].isin(filter_pos)) &
-                        (full_cut['team'].isin(filter_team))]
+                        (full_cut['Team'].isin(filter_team))]
+
+final_df.drop('Team', axis=1, inplace=True)
 
 final_df.to_csv('./data/GW'+str(crnt_gw)+'_preds.csv', index=False)
 
 home_away_dict = get_home_away_str_dict()
 
-total_fmt = {'Price': '£{:.1f}', 'Pred_Pts': '{:.1f}', 'TSB%': '{:.1%}'}
+total_fmt = {'Price': '£{:.1f}', 'xPts': '{:.1f}', 'TSB%': '{:.1%}'}
 st.dataframe(final_df.style.applymap(color_fixtures, subset=new_fixt_cols) \
              .format(total_fmt), height=1000, width=1000)
-
