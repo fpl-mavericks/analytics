4d8699805dafb205b3cc0fe0305a942251b1891b
Tim Youell <tim.youell@servian.com>
2024-08-15 12:12:38 +1000

    23/24 fixtures added

--- pages/2_🗓_Fixtures.py
+++ pages/2_🗓_Fixtures.py
@@ -12,18 +12,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pytz
 import datetime as datetime
-import os
-import sys
-
-file_dir = os.path.dirname(r'/Users/2279556/analytics/analytics/')
-sys.path.append(file_dir)
-
-# To-Do:
-# Update GF and GA plots to include tz_datetimes
-
-# Update GF and GA to not load until after events_df['GW1 finished'] == True
-# Have text telling to wait until after GW1 is finished for plots.
-
 
 from fpl_utils.fpl_api_collection import (
     get_bootstrap_data, get_current_gw, get_fixt_dfs, get_league_table
@@ -89,6 +77,9 @@ gw_numbers = range(slider1, slider2+1)
 gw_deadlines = events_df.loc[(events_df['id'] >= slider1) & (events_df['id'] <= slider2)]['tz_datetime']
 custom_labels = [f'GW{gw_number}\n{my_string}' for gw_number, my_string in zip(gw_numbers, gw_deadlines)]
 
+#Check to see if GW1 has finished in order to populate GF and GA plots.
+gw1_finished = events_df.loc[events_df['id'] == 1, 'finished'].values[0]
+
 # Fixture Difficulty Rating (FDR) seaborn plot
 if select_choice == 'Fixture Difficulty Rating (FDR)':
     st.write('The higher up the heatmap, the \'easier\' (according to the FDRs) the games in the selected GW range.')
@@ -130,7 +121,6 @@ if select_choice == 'Fixture Difficulty Rating (FDR)':
         annot_df = new_fixt_df
         sns.heatmap(new_fixt_df, ax=ax, annot=True, fmt='', cmap=flatui,
                     annot_kws={'size': annot_size}, cbar=False, linewidth=1, color='black')
-
     ax.set_xticks([x+0.5 for x in range(slider1-1, slider2)])
     ax.set_xticklabels(custom_labels, rotation=rotation, ha='center')
     plt.setp(ax.get_xticklabels(), fontsize=4)
@@ -139,86 +129,96 @@ if select_choice == 'Fixture Difficulty Rating (FDR)':
 
 # Average Goals Against seaborn plot
 elif select_choice == 'Average Goals Against (GA)':
-    st.write('The higher up the heatmap, based on historic averages, the higher chance of scoring in the selected GW range.')
-    filtered_team_df = team_fixt_df.iloc[:, slider1-1: slider2]
-    filtered_ga_df = team_ga_df.iloc[:, slider1-1:slider2]
-    ga_fixt_df = filtered_ga_df.copy()
-    ga_fixt_df.loc[:, 'fixt_ave'] = ga_fixt_df.mean(axis=1)
-    ga_fixt_df.sort_values('fixt_ave', ascending=False, inplace=True)
-    ga_fixt_df.drop('fixt_ave', axis=1, inplace=True)
-    ga_fixt_df = ga_fixt_df.astype(float)
-    filtered_team_df_ga = filtered_team_df.loc[ga_fixt_df.index]
-    
-    fig, ax = plt.subplots()
-    flatui_rev = ["#147d1b", "#00ff78", "#caf4bd", "#eceae6", "#fa8072", "#ff0057",
-              "#920947"][::-1]
-    if radio_choice == 'Fixture':
-        annot_df = filtered_team_df_ga
-        sns.heatmap(ga_fixt_df,
-                    ax=ax,
-                    cmap=flatui_rev,
-                    annot=False,
-                    fmt='',
-                    cbar=False,
-                    linewidth=1)
-        for i in range(len(annot_df)):
-            for j in range(slider2 - slider1+1):
-                val = annot_df[slider1 + j][list(annot_df[slider1 + j].keys())[i]]
-                g_val = ga_fixt_df[slider1 + j][list(ga_fixt_df[slider1 + j].keys())[i]]
-                if len(val) > 7:
-                    fontsize = annot_size/1.5
-                else:
-                    fontsize = annot_size
-                hash_color = map_float_to_color(g_val, flatui_rev, ga_fixt_df.min().min(), ga_fixt_df.max().max())
-                text_color = get_text_color_from_hash(hash_color)
-                plt.text(j + 0.5, i + 0.5, val, ha='center', va='center', fontsize=fontsize, color=text_color)
+    if gw1_finished:
+        st.write('The higher up the heatmap, based on historic averages, the higher chance of scoring in the selected GW range.')
+        filtered_team_df = team_fixt_df.iloc[:, slider1-1: slider2]
+        filtered_ga_df = team_ga_df.iloc[:, slider1-1:slider2]
+        ga_fixt_df = filtered_ga_df.copy()
+        ga_fixt_df.loc[:, 'fixt_ave'] = ga_fixt_df.mean(axis=1)
+        ga_fixt_df.sort_values('fixt_ave', ascending=False, inplace=True)
+        ga_fixt_df.drop('fixt_ave', axis=1, inplace=True)
+        ga_fixt_df = ga_fixt_df.astype(float)
+        filtered_team_df_ga = filtered_team_df.loc[ga_fixt_df.index]
+        
+        fig, ax = plt.subplots()
+        flatui_rev = ["#147d1b", "#00ff78", "#caf4bd", "#eceae6", "#fa8072", "#ff0057",
+                "#920947"][::-1]
+        if radio_choice == 'Fixture':
+            annot_df = filtered_team_df_ga
+            sns.heatmap(ga_fixt_df,
+                        ax=ax,
+                        cmap=flatui_rev,
+                        annot=False,
+                        fmt='',
+                        cbar=False,
+                        linewidth=1)
+            for i in range(len(annot_df)):
+                for j in range(slider2 - slider1+1):
+                    val = annot_df[slider1 + j][list(annot_df[slider1 + j].keys())[i]]
+                    g_val = ga_fixt_df[slider1 + j][list(ga_fixt_df[slider1 + j].keys())[i]]
+                    if len(val) > 7:
+                        fontsize = annot_size/1.5
+                    else:
+                        fontsize = annot_size
+                    hash_color = map_float_to_color(g_val, flatui_rev, ga_fixt_df.min().min(), ga_fixt_df.max().max())
+                    text_color = get_text_color_from_hash(hash_color)
+                    plt.text(j + 0.5, i + 0.5, val, ha='center', va='center', fontsize=fontsize, color=text_color)
+        else:
+            annot_df = ga_fixt_df
+            sns.heatmap(annot_df, ax=ax, annot=True, fmt='', cmap=flatui_rev,
+                        annot_kws={'size': annot_size}, cbar=False, linewidth=1, color='black')
+        ax.set_xticks([x+0.5 for x in range(slider1-1, slider2)])
+        ax.set_xticklabels(custom_labels, rotation=rotation, ha='center')
+        plt.setp(ax.get_xticklabels(), fontsize=4)
+        ax.set_ylabel('Team')
+        st.write(fig)
     else:
-        annot_df = ga_fixt_df
-        sns.heatmap(annot_df, ax=ax, annot=True, fmt='', cmap=flatui_rev,
-                    annot_kws={'size': annot_size}, cbar=False, linewidth=1, color='black')
-    ax.set_xlabel('Gameweek')
-    ax.set_ylabel('Team')
-    st.write(fig)
+        st.write('Please wait until GW1 concludes to view Goals Against metrics.')
 
 # Average Goals For seaborn plot
 elif select_choice == 'Average Goals For (GF)':
-    st.write('The higher up the heatmap, based on historic averages, the higher chance of keeping a clean sheet in the selected GW range.')
-    filtered_team_df = team_fixt_df.iloc[:, slider1-1: slider2]
-    filtered_gf_df = team_gf_df.iloc[:, slider1-1:slider2]
-    gf_fixt_df = filtered_gf_df.copy()
-    gf_fixt_df.loc[:, 'fixt_ave'] = gf_fixt_df.mean(axis=1)
-    gf_fixt_df.sort_values('fixt_ave', ascending=True, inplace=True)
-    gf_fixt_df.drop('fixt_ave', axis=1, inplace=True)
-    gf_fixt_df = gf_fixt_df.astype(float)
-    filtered_team_df_gf = filtered_team_df.loc[gf_fixt_df.index]
-    
-    fig, ax = plt.subplots()
-    flatui = ["#147d1b", "#00ff78", "#caf4bd", "#eceae6", "#fa8072", "#ff0057",
-              "#920947"]
-    if radio_choice == 'Fixture':
-        annot_df = filtered_team_df_gf
-        sns.heatmap(gf_fixt_df,
-                    ax=ax,
-                    cmap=flatui,
-                    annot=False,
-                    fmt='',
-                    cbar=False,
-                    linewidth=1)
-        for i in range(len(annot_df)):
-            for j in range(slider2 - slider1+1):
-                val = annot_df[slider1 + j][list(annot_df[slider1 + j].keys())[i]]
-                g_val = gf_fixt_df[slider1 + j][list(gf_fixt_df[slider1 + j].keys())[i]]
-                if len(val) > 7:
-                    fontsize = annot_size/1.5
-                else:
-                    fontsize = annot_size
-                hash_color = map_float_to_color(g_val, flatui, gf_fixt_df.min().min(), gf_fixt_df.max().max())
-                text_color = get_text_color_from_hash(hash_color)
-                plt.text(j + 0.5, i + 0.5, val, ha='center', va='center', fontsize=fontsize, color=text_color)
+    if gw1_finished:
+        st.write('The higher up the heatmap, based on historic averages, the higher chance of keeping a clean sheet in the selected GW range.')
+        filtered_team_df = team_fixt_df.iloc[:, slider1-1: slider2]
+        filtered_gf_df = team_gf_df.iloc[:, slider1-1:slider2]
+        gf_fixt_df = filtered_gf_df.copy()
+        gf_fixt_df.loc[:, 'fixt_ave'] = gf_fixt_df.mean(axis=1)
+        gf_fixt_df.sort_values('fixt_ave', ascending=True, inplace=True)
+        gf_fixt_df.drop('fixt_ave', axis=1, inplace=True)
+        gf_fixt_df = gf_fixt_df.astype(float)
+        filtered_team_df_gf = filtered_team_df.loc[gf_fixt_df.index]
+        
+        fig, ax = plt.subplots()
+        flatui = ["#147d1b", "#00ff78", "#caf4bd", "#eceae6", "#fa8072", "#ff0057",
+                "#920947"]
+        if radio_choice == 'Fixture':
+            annot_df = filtered_team_df_gf
+            sns.heatmap(gf_fixt_df,
+                        ax=ax,
+                        cmap=flatui,
+                        annot=False,
+                        fmt='',
+                        cbar=False,
+                        linewidth=1)
+            for i in range(len(annot_df)):
+                for j in range(slider2 - slider1+1):
+                    val = annot_df[slider1 + j][list(annot_df[slider1 + j].keys())[i]]
+                    g_val = gf_fixt_df[slider1 + j][list(gf_fixt_df[slider1 + j].keys())[i]]
+                    if len(val) > 7:
+                        fontsize = annot_size/1.5
+                    else:
+                        fontsize = annot_size
+                    hash_color = map_float_to_color(g_val, flatui, gf_fixt_df.min().min(), gf_fixt_df.max().max())
+                    text_color = get_text_color_from_hash(hash_color)
+                    plt.text(j + 0.5, i + 0.5, val, ha='center', va='center', fontsize=fontsize, color=text_color)
+        else:
+            annot_df = gf_fixt_df
+            sns.heatmap(annot_df, ax=ax, annot=True, fmt='', cmap=flatui,
+                        annot_kws={'size': annot_size}, cbar=False, linewidth=1, color='black')
+        ax.set_xticks([x+0.5 for x in range(slider1-1, slider2)])
+        ax.set_xticklabels(custom_labels, rotation=rotation, ha='center')
+        plt.setp(ax.get_xticklabels(), fontsize=4)
+        ax.set_ylabel('Team')
+        st.write(fig)
     else:
-        annot_df = gf_fixt_df
-        sns.heatmap(annot_df, ax=ax, annot=True, fmt='', cmap=flatui,
-                    annot_kws={'size': annot_size}, cbar=False, linewidth=1, color='black')
-    ax.set_xlabel('Gameweek')
-    ax.set_ylabel('Team')
-    st.write(fig)
+        st.write('Please wait until GW1 concludes to view Goals Against metrics.')
